2262322d3edd4fefdb7eae12d334244ef04bd450
OfentseThulare <Ofentsethulare1@gmail.com>
2026-04-18 02:28:06 +0200

    style(deck): lift page 30 content-system image 5px so it clears the footer

--- src/slides/Slide24ContentSystem.tsx
+++ src/slides/Slide24ContentSystem.tsx
@@ -72,6 +72,7 @@ export default function Slide24ContentSystem() {
               width: 'auto',
               height: 'auto',
               display: 'block',
+              transform: 'translateY(-5px)',
             }}
           />
         </div>
